d8200a4cce9313c11ff186e1f7733180457ed052
MrTubbz <nathadawson123@hotmail.co.uk>
2022-05-28 00:47:27 +0100

    All the stuff

--- model/openai_test.py
+++ model/openai_test.py
@@ -7,13 +7,13 @@ openai.api_key = cfg["OPENAI_API_KEY"]
 
 if __name__ == "__main__":
     response = openai.Completion.create(
-        model="ada:ft-personal:random-prompts-2022-05-24-17-54-23",
+        model="ada:ft-personal:comined-improved-2022-05-25-00-52-07",
         prompt=input("Promt: ") + " ->",
         stop=" END",
         temperature=0.1,
         max_tokens=250,
-        presence_penalty=1.5,
-        frequency_penalty=1.5
+        presence_penalty=1.8,
+        frequency_penalty=1.8
     )
 
     print(response["choices"][0]["text"])
--- promt_extraction/promt_combining.py
+++ promt_extraction/promt_combining.py
@@ -1,10 +1,12 @@
 import json
 import re
+import random
+import time
 
 def check_duplicate(x1, x2):
     for n1 in x1:
         for n2 in x2:
-            if n1 == n2:
+            if n1.split(" ").pop(0) == n2.split(" ").pop(0):
                 return True
             # endif
         # endfor
@@ -19,34 +21,35 @@ with open("./extracted_prompts.json", "r") as f:
     data = json.load(f)
 # endwith
 
+delimit = "."
+suffix = " END"
+max_length = random.randrange(1,11)
+
 prompt_collection = []
 for d1 in data:
     d3 = d1
-    delimit = "."
-    suffix = " END"
     prompts1 = [p + delimit for p in d1["prompt"].split(delimit)][:-1]
     for d2 in data:
         if d1 != d2:
             prompts2 = [p + delimit for p in d2["prompt"].split(delimit)][:-1]
             dup = check_duplicate(prompts1, prompts2)
-            if len(d3["prompt"].split(delimit)) > 5:
-                if suffix not in d3["completion"]:
-                    d3["completion"] += suffix
-                prompt_collection.append(d3)
-                d3 = d1
-            # endif
             if not dup:
+                if len(d3["prompt"].split(delimit)) > max_length:
+                    prompt_collection.append(d3)
+                    d3 = d1
+                    max_length = random.randrange(1,11)
+                # endif
                 d3 = {"prompt":"".join(prompts1) + "".join(prompts2) + "->", "completion":d3["completion"] + d2["completion"]}
+                prompts1= [p + delimit for p in d3["prompt"].split(delimit)][:-1]
             # endif
         # endif
     # endfor
-    if suffix not in d3["completion"]:
-        d3["completion"] += suffix
     prompt_collection.append(d3)
 # endfor
 
 for p in prompt_collection:
     p["prompt"] = one_space(p["prompt"])
+    p["completion"] += suffix
 
 with open("combined_prompts.json", "w") as f:
     json.dump(prompt_collection, f)
